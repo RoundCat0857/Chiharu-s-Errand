afff683838e5b515a002246fe92dd2f40fe13e44
RoundCat0857 <round0857cat@gmail.com>
2018-11-30 20:16:04 +0900

    delete not needed stage

--- src/menu/main.js
+++ src/menu/main.js
@@ -5,7 +5,6 @@ window.onload = function () {
   core.preload('../../image/scene/menu.png', '../../image/texture/message.png')
   core.fps = 10
   core.onload = function() {
-    const stage = new Group()
     startMenu(core)
   }
   core.start()
