aaf6c3359fa7082a85832c3ff011de797b995d5e
RoundCat0857 <round0857cat@gmail.com>
2018-12-10 17:51:32 +0900

    chapter1

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -1,6 +1,20 @@
 enchant()
+// document.write("<script type='text/javascript' src='./menu/menu.js'></script>")
 
-function chapter1(core) {
-  console.log(maps)
+function chapter1(core, stage) {
+  const map = maps.chapter1
+  const images = ['texture/interior1.png']
+  // core.load('../image/' + images[0])
+  // setPreload(core, images)
+  const startScene = startChapter(1)
+  // core.currentScene(startScene)
+  if (!stage || stage === 'chiharuRoom') {
+    const chiharuRoom = setMap(core, map['chiharuRoom'], images[0])
+    const stage = new Scene()
+    stage.addChild(chiharuRoom)
+    console.log(stage)
+    core.currentScene.addChild(stage)
+    core.pushScene(stage)
+  }
   console.log(core)
 }
