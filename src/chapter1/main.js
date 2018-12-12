215dce9bd00fc50d492c769bf0977c0ea502fcd6
RoundCat0857 <round0857cat@gmail.com>
2018-12-12 18:13:46 +0900

    rewrite function

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -1,18 +1,12 @@
 enchant()
-// document.write("<script type='text/javascript' src='./menu/menu.js'></script>")
 
 function chapter1(core, stage) {
   const map = maps.chapter1
   const images = ['texture/interior1.png']
-  // core.load('../image/' + images[0])
-  // setPreload(core, images)
   const startScene = startChapter(1)
-  // core.currentScene(startScene)
   if (!stage || stage === 'chiharuRoom') {
-    const chiharuRoom = setMap(core, map['chiharuRoom'], images[0])
-    const stage = new Scene()
-    stage.addChild(chiharuRoom)
-    core.currentScene.addChild(stage)
-    core.pushScene(stage)
+    const chiharuRoom = setStage(core, map['chiharuRoom'], images[0])
+    core.currentScene.addChild(chiharuRoom)
+    core.pushScene(chiharuRoom)
   }
 }
