56188d526b2eb2ab2819a7e2d76aca0aba76f1dd
RoundCat0857 <round0857cat@gmail.com>
2018-12-16 00:58:03 +0900

    start

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -1,12 +1,20 @@
 enchant()
 
 function chapter1(core, stage) {
-  const map = maps.chapter1
-  const images = ['texture/interior1.png']
+  const map1 = maps.chapter1
+  const images = ['texture/interior1.png', 'texture/char_p03.png']
   const startScene = startChapter(1)
   if (!stage || stage === 'chiharuRoom') {
-    const chiharuRoom = setStage(core, map['chiharuRoom'], images[0])
-    core.currentScene.addChild(chiharuRoom)
-    core.pushScene(chiharuRoom)
+    const chiharuRoom = map1['chiharuRoom']
+    const { start, next } = chiharuRoom
+    const [map, forergoundMap] = setStage(core, chiharuRoom, images[0])
+    const stage = new Scene()
+    stage.addChild(map)
+    stage.addChild(forergoundMap)
+    const chiharu = new Human(core, 'Chiharu', images[1], start, 1, 7)
+    walk(core, map, chiharu)
+    stage.addChild(chiharu)
+    core.currentScene.addChild(stage)
+    core.pushScene(stage)
   }
 }
