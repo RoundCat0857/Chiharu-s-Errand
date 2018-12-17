63e9674dfebc9749a9046cb1579f763b819479f7
RoundCat0857 <round0857cat@gmail.com>
2018-12-17 19:45:07 +0900

    remove startScene

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -3,15 +3,16 @@ enchant()
 function chapter1(core, stageName) {
   const map1 = maps.chapter1
   const images = ['texture/interior1.png', 'texture/char_p03.png']
-  const startScene = startChapter(1)
   if (!stageName || stageName === 'chiharuRoom') {
     const chiharuRoom = map1['chiharuRoom']
     const { start, stairs } = chiharuRoom
     const [map, forergoundMap] = setStage(core, chiharuRoom, images[0])
-    const chiharu = new Human(core, 'Chiharu', images[1], start, 1, 7)
-    if (stageName === 'chiharuRoom') {
-      chiharu.x = 11 * 16 + 12
-      chiharu.y = 10 * 16
+    let chiharu = {}
+    if (!stageName) {
+      const startScene = startChapter(1)
+      chiharu = new Human(core, 'Chiharu', images[1], start, 1, 7)
+    } else {
+      chiharu = new Human(core, 'Chiharu', images[1], stairs[2], 1, 7)
     }
     const stage = new Scene()
     addChilds(stage, [map, forergoundMap, chiharu])
