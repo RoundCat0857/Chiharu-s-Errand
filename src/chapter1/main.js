0c0533743a2989a31120410a29a1984f8695ec58
RoundCat0857 <round0857cat@gmail.com>
2018-12-17 19:37:45 +0900

    start from stairs

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -26,9 +26,9 @@ function chapter1(core, stageName) {
     keepMapCenter(core, stage, map, chiharu)
   } else if (stageName === 'living') {
     const living = map1['living']
-    const { start, stairs } = living
+    const { stairs, door } = living
     const [map, forergoundMap] = setStage(core, living, images[0])
-    const chiharu = new Human(core, 'Chiharu', images[1], start, 1, 7)
+    const chiharu = new Human(core, 'Chiharu', images[1], stairs[1], 1, 7)
     const stage = new Scene()
     addChilds(stage, [map, forergoundMap, chiharu])
     core.pushScene(stage)
