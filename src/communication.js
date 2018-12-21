f4b7b5c0d655ee644bb73d17870576e02cdd0646
RoundCat0857 <round0857cat@gmail.com>
2018-12-21 11:29:55 +0900

    first commit

---
+++ src/communication.js
@@ -0,0 +1,10 @@
+const communications = {
+  chapter1: {
+    firstTalk: {
+      1: {
+        text: 'お母さん、カレーに入れるにんじんを買ってくるのを忘れて<br>しまったみたいなの。ちぃちゃん買ってきてくれないかしら。',
+        speaker: 'お母さん'
+      }
+    }
+  }
+}
